9a841d7a979999242bc0a3faeb5d0f9964bf2037
Nancy El-Sharkawi <Nancy@Nancys-MacBook-Pro.local>
2020-10-02 13:50:38 +0300

    settings

--- scripts.js
+++ scripts.js
@@ -1,23 +1,17 @@
 const counter = document.querySelector(".counterButton");
 
-// 👇🏻 مكان النقاط count قم بتعيين القيمة صفر إلى المتغير
-// ...
+let count = 0;
 
 counter.addEventListener("click", incrementCounter);
 
-/******************************************************
- *  هي أداة تقوم بتكرار الكود الذي incrementCounter()  *
- *  تكتبه داخلها كلما ضغطت على الزر                    *
- ******************************************************/
-
 function incrementCounter() {
-  // 👇🏻 بواحد مكان النقاط count قم بزيادة قيمة
-  // ...
+  count++;
   counter.innerHTML = count;
-  // "تسوي 1 اطبع "جيد count إن كانت قيمة العداد أو
-  if (condition) {
+  if (count == 1) {
     document.getElementById("status").innerHTML = "جيد";
+  } else if (count > 1 && count < 6) {
+    document.getElementById("status").innerHTML = "جيد جداً";
+  } else if (count >= 6) {
+    document.getElementById("status").innerHTML = "رائع";
   }
-  // "أكبر من 1 وأقل من 6 اطبع "جيد جداً count إن كانت قيمة العداد أو
-  // "أكبر من أو تساوي 6 اطبع "رائع count إن كانت قيمة العداد أو
 }
